b90c29d6132ba86a0ad553a045059bc2245aae6c
Mike Fulton <mikefultonpersonal@gmail.com>
2023-11-29 23:38:10 -0500

    baseline - fails on link

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# GMP
+gmp
+
+GMP is a free library for arbitrary precision arithmetic, operating on signed integers, rational numbers, and floating-point numbers.
---
+++ cicd-stable.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/gmpport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/gmpport.git'), string(name: 'PORT_DESCRIPTION', value: 'GMP is a free library for arbitrary precision arithmetic, operating on signed integers, rational numbers, and floating-point numbers.' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+  }
+}
